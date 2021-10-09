468f34f99b484a1777f26791d592903e13fa542c
JudeCoin <13635607098@163.com>
2021-10-09 12:51:22 +0800

    set seed language in generate_from_keys

--- src/mnemonics/electrum-words.cpp
+++ src/mnemonics/electrum-words.cpp
@@ -194,7 +194,6 @@ namespace
   {
     epee::wipeable_string trimmed_words = "", word;
 
-    const auto &word_map = language->get_word_map();
     const auto &trimmed_word_map = language->get_trimmed_word_map();
     const uint32_t unique_prefix_length = language->get_unique_prefix_length();
     for (std::vector<epee::wipeable_string>::const_iterator it = word_list.begin(); it != word_list.end(); it++)
@@ -492,6 +491,14 @@ namespace crypto
       return "<language not found>";
     }
 
+    bool is_valid_language(const std::string &language)
+    {
+      const std::vector<const Language::Base*> language_instances = get_language_list();
+      for (std::vector<const Language::Base*>::const_iterator it = language_instances.begin(); it != language_instances.end(); it++)
+        if ((*it)->get_english_language_name() == language || (*it)->get_language_name() == language)
+          return true;
+      return false;
+    }
   }
 
 }
