c7cc1f592c378596da97b80301adb947f4d4e4ce
judecoin <13635607098@163.com>
2023-01-15 01:12:15 +0800

    Bump ring size to 16 for v15 & remove set default in wallet cli

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -1177,8 +1177,7 @@ namespace cryptonote
       return false;
     }
 
-    const uint8_t hf_version = m_blockchain_storage.get_current_hard_fork_version();
-    if (!check_tx_inputs_ring_members_diff(tx, hf_version))
+    if (!check_tx_inputs_ring_members_diff(tx))
     {
       MERROR_VER("tx uses duplicate ring members");
       return false;
@@ -1187,12 +1186,6 @@ namespace cryptonote
     if (!check_tx_inputs_keyimages_domain(tx))
     {
       MERROR_VER("tx uses key image not in the valid domain");
-      return false;
-    }
-
-    if (!check_output_types(tx, hf_version))
-    {
-      MERROR_VER("tx does not use valid output type(s)");
       return false;
     }
 
@@ -1302,9 +1295,10 @@ namespace cryptonote
     return true;
   }
   //-----------------------------------------------------------------------------------------------
-  bool core::check_tx_inputs_ring_members_diff(const transaction& tx, const uint8_t hf_version) const
+  bool core::check_tx_inputs_ring_members_diff(const transaction& tx) const
   {
-    if (hf_version >= 6)
+    const uint8_t version = m_blockchain_storage.get_current_hard_fork_version();
+    if (version >= 6)
     {
       for(const auto& in: tx.vin)
       {
