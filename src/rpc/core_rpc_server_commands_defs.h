becefa1ca67e937486a7151d4319b1c5a709fd62
public-new <public-new@outlook.com>
2024-10-18 09:20:05 +0800

    core_rpc_server: return ID of submitted block

--- src/rpc/core_rpc_server_commands_defs.h
+++ src/rpc/core_rpc_server_commands_defs.h
@@ -88,7 +88,7 @@ namespace cryptonote
 // advance which version they will stop working with
 // Don't go over 32767 for any of these
 #define CORE_RPC_VERSION_MAJOR 3
-#define CORE_RPC_VERSION_MINOR 12
+#define CORE_RPC_VERSION_MINOR 13
 #define MAKE_CORE_RPC_VERSION(major,minor) (((major)<<16)|(minor))
 #define CORE_RPC_VERSION MAKE_CORE_RPC_VERSION(CORE_RPC_VERSION_MAJOR, CORE_RPC_VERSION_MINOR)
 
@@ -147,11 +147,13 @@ namespace cryptonote
 
     struct response_t: public rpc_response_base
     {
+      std::string block_id;
       uint64_t 	 height;
       std::string hash;
 
       BEGIN_KV_SERIALIZE_MAP()
         KV_SERIALIZE_PARENT(rpc_response_base)
+        KV_SERIALIZE(block_id)
         KV_SERIALIZE(height)
         KV_SERIALIZE(hash)
       END_KV_SERIALIZE_MAP()
