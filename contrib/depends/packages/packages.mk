db4e2cbb988455256cb9cab64f8d1a9c3949e072
Judith250 <Judith01230@outlook.com>
2025-03-14 13:08:40 +0800

    depends: move sodium to packages

--- contrib/depends/packages/packages.mk
+++ contrib/depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl zeromq expat unbound
+packages:=boost openssl zeromq expat unbound sodium
 
 # ccache is useless in gitian builds
 ifneq ($(GITIAN),1)
@@ -9,23 +9,23 @@ hardware_packages := hidapi protobuf libusb
 hardware_native_packages := native_protobuf
 
 android_native_packages = android_ndk $(hardware_native_packages)
-android_packages = ncurses readline sodium protobuf
+android_packages = ncurses readline protobuf
 
 darwin_native_packages = $(hardware_native_packages)
-darwin_packages = ncurses readline sodium $(hardware_packages)
+darwin_packages = ncurses readline $(hardware_packages)
 
 # not really native...
 freebsd_native_packages = freebsd_base $(hardware_native_packages)
-freebsd_packages = ncurses readline sodium protobuf libusb
+freebsd_packages = ncurses readline protobuf libusb
 
-linux_packages = eudev ncurses readline sodium $(hardware_packages)
+linux_packages = eudev ncurses readline $(hardware_packages)
 linux_native_packages = $(hardware_native_packages)
 
 ifeq ($(build_tests),ON)
 packages += gtest
 endif
 
-mingw32_packages = sodium $(hardware_packages)
+mingw32_packages = $(hardware_packages)
 mingw32_native_packages = $(hardware_native_packages)
 
 ifneq ($(build_os),darwin)
