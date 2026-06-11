22c9b20489e157e208a342f53075afa959ee644d
blockSkyLook <shangdiainilong@outlook.com>
2026-06-11 21:48:55 +0800

    Misc clang 21 fixes

--- src/serialization/pair.h
+++ src/serialization/pair.h
@@ -29,7 +29,7 @@
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
 #pragma once
-#include <memory>
+#include <utility>
 #include <boost/type_traits/make_unsigned.hpp>
 #include "serialization.h"
 
