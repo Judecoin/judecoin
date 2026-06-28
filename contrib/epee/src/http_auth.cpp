af1e4e5652da8dfe0a2e69aa7e0c6d04ab0e0af2
public-new <public-new@outlook.com>
2026-06-28 10:57:00 +0800

    Harden HTTP client auth

--- contrib/epee/src/http_auth.cpp
+++ contrib/epee/src/http_auth.cpp
@@ -64,6 +64,7 @@
 #include <iterator>
 #include <limits>
 #include <openssl/evp.h>
+#include <openssl/rand.h>
 #include <tuple>
 #include <type_traits>
 
@@ -284,6 +285,11 @@ namespace
         "number of digits will cause underflow on padding below"
       );
 
+      std::array<unsigned char, 16> rbuf{{}};
+      if (RAND_bytes(rbuf.data(), rbuf.size()) != 1)
+        return {};
+      const std::string cnonce = epee::string_encoding::base64_encode(rbuf.data(), rbuf.size());
+
       std::string out{};
       out.reserve(client_reserve_size);
 
@@ -295,10 +301,11 @@ namespace
       std::array<char, 8> nc{{}};
       boost::copy(out, nc.data());
       const auto response = digest(
-        generate_a1(digest, user), u8":", user.server.nonce, u8":", nc, u8"::auth:", digest(method, u8":", uri)
+        generate_a1(digest, user), u8":", user.server.nonce, u8":", nc, u8":", cnonce, u8":auth:", digest(method, u8":", uri)
       );
       out.clear();
       init_client_value(out, Digest::name, user, uri, response);
+      add_field(out, u8"cnonce", quoted_(cnonce));
       add_field(out, u8"qop", ceref(u8"auth"));
       add_field(out, u8"nc", nc);
       return out;
@@ -590,7 +597,10 @@ namespace
           boost::equals((*digest).name, request.algorithm, ascii_iequal)
         );
         if (request.qop.empty())
+        {
           value_generator = old_algorithm<digest_type>{*digest};
+          index = boost::fusion::size(digest_algorithms);
+        }
         else
         {
           for (auto elem = boost::make_split_iterator(request.qop, boost::token_finder(http_list_separator));
